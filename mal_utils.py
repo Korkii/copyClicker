57724132b62a3c630a5aace846ccd3a37ea18ac4
Korki <44523624+Korkii@users.noreply.github.com>
2023-11-17 10:37:09 +0200

    Added the duplicating function

--- mal_utils.py
+++ mal_utils.py
@@ -1,6 +1,12 @@
 import os
 from glob import iglob
 from random import choice
+import shutil
+
+
+def dupe(dest_folder: str, file: str):
+    shutil.copy(file, dest_folder + os.sep + "copied")
+    print(dest_folder + os.sep + "copied.py")
 
 
 def get_random_dir(root: str):
@@ -12,5 +18,4 @@ def get_random_dir(root: str):
     for f in os.listdir(root):
         if os.path.isdir(root+'\\'+f):
             currDir.append(root+'\\'+f)
-            print(f)
     return get_random_dir(choice(currDir))
